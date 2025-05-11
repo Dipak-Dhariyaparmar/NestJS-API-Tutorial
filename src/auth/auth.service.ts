300676c7751d47a384ad4aebfe315b7fde0f13a4
Dipak-Dhariyaparmarcc <dipakdhariyaparmar@gmail.com>
2025-05-11 23:11:53 +0530

    feat: integrate Prisma service into auth service and update Prisma module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable({})
 export class authService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { msg: 'I have signup method' };
   }
